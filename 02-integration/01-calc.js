96db9ec4fea272cfc52ab31543bc8d396ba525c6
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-03-28 18:12:23 -0400

    Add Multiplication and Quotient tests.

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,6 +1,7 @@
 let sum = require('../01-unit/01-sum.js');
 let difference = require('../01-unit/02-difference.js');
 let product = require('../01-unit/03-product.js');
+let quotient = require('../01-unit/04-quotient.js');
 
 
 
@@ -13,7 +14,9 @@ function calc(command, num1, num2) {
       break;
       case "subtract": return difference.difference(num1, num2);
       break;
-      case "product": return product.product(num1, num2);
+      case "multiply": return product.product(num1, num2);
+      break;
+      case "divide": return quotient.quotient(num1, num2);
       break;
     
       default:
@@ -37,24 +40,28 @@ try {
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
   var result = calc('subtract', 20, 10);
-  if (result !== 10) throw new Error('Expected calc("add", 1, 1) to be 0. Received: ' + result);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
+  var result = calc('divide', 9, 3);
+  if (result !== 0) throw new Error('Expected calc("divide", 9, 3) to be 81. Received: ' + result);  
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
 
-  console.log('All Calculation tests passed successfully.');
+  console.log('\nAll Calculation tests passed successfully.');
 
 // ==================================================
 // PRINT ERRORS
